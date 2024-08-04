1779abdb89d1bf961988c17942f64ca4d653dcd0
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-08-04 18:56:54 +0300

    last test

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,20 +1,20 @@
 export const Bio = {
-    name: "Joshua Ng'apos;ang'&apos;a",
+    name: "Joshua Ng&apos;ang&apos;a",
     roles: [
       "...Full Stack Developer",
-      "...Android Developer",
-      "...UI/UX Designer",
-      "...Programmer",
+      "...Technical Writer",
+      "...UI/UX Design",
+      
     ],
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-    github: "https://github.com/rishavchanda",
+    github: "https://github.com/RafasGit",
     resume:
       "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
-    linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-    twitter: "https://twitter.com/RishavChanda",
+    linkedin: "https://www.linkedin.com/in/joshua-ng-ang-a-13158120a",
+    twitter: "https://x.com/rafa_codes22",
     insta: "https://www.instagram.com/rishav_chanda/",
-    facebook: "https://www.facebook.com/rishav.chanda.165/",
+  
   };
 
   export const skills = [
