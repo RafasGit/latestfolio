4045774f3afe960fc884af75e8163403b2b9ef89
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-08-06 21:52:57 +0300

    last test

--- src/components/sections/skills/skills.jsx
+++ src/components/sections/skills/skills.jsx
@@ -43,10 +43,7 @@ const Skills = () => {
             established IT company with the opportunity to work with the latest
             technologies on challenging and diverse projects.
           </p>
-          <p align="LEFT">
-            I'm quiet confident, naturally curious, and perpetually working on
-            improving my chops one design problem at a time.
-          </p>
+        
           <p>
             If I need to define myself in one sentence that would be a family
             person, father of a beautiful daughter, a sports fanatic,
