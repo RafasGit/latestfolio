83ca3b52dd11abf34ac6e0f4f7c55e800a018ae2
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-08-07 17:02:43 +0300

    last test

--- src/components/sections/skills/skills.jsx
+++ src/components/sections/skills/skills.jsx
@@ -39,15 +39,20 @@ const Skills = () => {
           </h1>
           <div className="paras">
           <p>
-           I am a software engineer with three years experience in the development of fullstack applications, primarily in TypeScript, Tanstack and NextJs.
-           I was priviledged with the opportunity to begin my career in tech by interning at the  developer training agency <a href="" >100Devs </a>;        
-          </p>
-        
+           I am a software engineer and technical writer with three years of experience in the development of fullstack applications, primarily in TypeScript, Tanstack and NextJs. <br />
+            <br />
+             I focus on delivering projects efficiently without compromising on quality and that the first iteration should never be the last; there is always room for improvement.     
+          </p>          
           <p>
-            If I need to define myself in one sentence that would be a family
-            person, father of a beautiful daughter, a sports fanatic,
-            photography enthusiast, and tech-obsessed!!!
+          I was priviledged with the opportunity to  transition into tech by interning at the  <a href="https://100devs.org/" target="_blank" >100Devs </a> ; a US based developer training agency. This opportunity enabled me to contribute and collaborate on both real-world Open Source and Proprietary MERN(Mongo, Express, React and Node) stack projects.
           </p>
+          <p>
+         I have since used the foundational knowledge gained at the 100Devs to kickstart my curiosity and build individual <a href="#Projects" >projects </a> while exploring various technlogies and frameworks such as Next.js, ORMs such as Drizzle and stacks such as the T3 Stack(TypeScript, tRPC, NextAuth and Tailwind).  </p>
+          <p>
+          In my enthusiasm for the latest technologies and as a firm believer of teaching the things we know to understand what we don&apos;t, I have also written several <a href="#Projects" >articles </a> on first-principle topics mean&apos;t to help professionals transitioning into tech.   
+         
+          </p>
+
           </div>
         
         </div>
