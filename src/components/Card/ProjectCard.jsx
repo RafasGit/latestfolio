90d58b2bf4787d3ed84a3f70abcddd7e21a0d281
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-08-24 21:45:39 +0300

    first commit

--- src/components/Card/ProjectCard.jsx
+++ src/components/Card/ProjectCard.jsx
@@ -132,7 +132,7 @@ const ProjectCard = ({ project, setOpenModal }) => {
         <Members>
           {project.member?.map((member) => (
             <Avatar src={member.img} />
-          ))} the quick brown fo
+          ))} the quick brown fox
         </Members>
       </Card>
     );
